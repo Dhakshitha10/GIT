f5f7c57b4033e31c2d9e6b37b4eaba7f51527e46
Dhakshitha10 <dhakshitha2824@gmail.com>
2025-06-06 15:10:52 +0530

    dha

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log(5==='5');
+// let i=0
+// do{
+//     i++;
+//     console.log(i);
+//  }while(i<5)
+// function greet(name,age){
+
+//     return  `hello ${name} is ${age} old`
+// }
+//console.log (greet('ronaldo',40));
+// function greet(a,b){
+//     let sum=a+b;
+//     console.log("the sum is:"+sum);
+// }
+// (28,24)
+function game(userchoice){
+            const choices=['rock','paper','scissor']
+            let computer =choices[math.floor(math.random()*3)]
+        }
